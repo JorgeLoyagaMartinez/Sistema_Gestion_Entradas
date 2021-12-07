eb5427541b2b2de65fbb5d38316d9ce52967dccc
vsol <vsol.sil@gmail.com>
2021-12-07 05:08:02 -0300

    metodo index, create, store y show en EventoController
    pruebas en vistas eventos, create y show en blade

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,17 +13,31 @@ class EventoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $nombre = $request->get('nombre');
         
-        //devuelve array de objetos iguales a la base datos que pedi
-        $Eventos = DB::select("SELECT * FROM evento");
+        if ($request->has('nombre')){
+            $busqueda = 'Resultados para: '.$nombre;
+            // $eventos = DB::table('eventos')
+            //             ->select('*')
+            //             ->where('nombre', 'like', '%'.$nombre.'%')
+            //             ->get();
+            $eventos = Event::where('nombre', 'like', '%'.$nombre.'%')->get();
+        } else {
+            $busqueda = 'Todos los eventos';
+            // $eventos = DB::table('eventos')
+            //             ->select('*')
+            //             ->get();
+            $eventos = Event::get();
+        }
+
         $parametro = [
-            "eventos" => $Eventos,
-            "titulo" => "esto es la tabla completa de Eventos"
+            'eventos' => $eventos,
+            'titulo' => $busqueda
         ];
+
         return view('eventos.eventos', $parametro);
-        //return "Esto es el index del EventoController";
     }
 
     /**
@@ -32,7 +47,7 @@ class EventoController extends Controller
      */
     public function create()
     {
-        //
+        return view('eventos.create');
     }
 
     /**
@@ -43,7 +58,27 @@ class EventoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+    
+        // foreach($evento as $clave => $valor) {
+        //     DB::insert($clave, $valor);            
+        // }
+
+        Event::create([
+            'nombre' => $request->get('nombre'),
+            'descripcion' => $request->get('descripcion'),
+            'portada' => $request->get('portada'),
+            'fecha' => $request->get('fecha'),
+            'lugar' => $request->get('lugar'),
+            'horario' => $request->get('horario'),
+            'precio' => $request->get('precio'),
+            'stock' => $request->get('stock'),
+            'estado' => $request->get('estado')
+        ]);
+
+        return redirect()->route('eventos.eventos');
+
+    
+            
     }
 
     /**
@@ -54,7 +89,9 @@ class EventoController extends Controller
      */
     public function show($id)
     {
-        //
+        $evento = Event::findOrFail($id);
+
+        return view('eventos.show', ['evento'=>$evento]);
     }
 
     /**
---
+++ resources/views/eventos/create.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.0/css/mdb.min.css'>
+    <title>Document</title>
+</head>
+<body class="container my-4">
+    <h1>Gestión de eventos</h1>
+    <h2>Búsqueda de eventos</h2>
+
+
+    <section>
+        <h3>Registro de eventos</h3>
+        <form class="row g-3" action="{{ route('eventos.store')}}" method="POST">
+            @csrf
+            <div class="col-md-12">
+              <label for="nombre" class="form-label">Nombre</label>
+              <input type="text" class="form-control" id="nombre" name='nombre' placeholder="Nombre del evento">
+            </div>
+            <div class="col-md-12">
+              <label for="descripcion" class="form-label">Descripción</label>
+              <textarea class="form-control" id="descripcion" name='descripcion' placeholder="Descripción del evento"></textarea>
+            </div>
+            <div class="col-12">
+              <label for="portada" class="form-label">Imagen de portada</label>
+              <input type="text" class="form-control" id="portada" name='portada'>
+            </div>
+            <div class="col-12">
+              <label for="lugar" class="form-label">Lugar</label>
+              <input type="text" class="form-control" id="lugar" name='lugar'>
+            </div>
+            <div class="col-6">
+              <label for="fecha" class="form-label">Fecha</label>
+              <input type="date" class="form-control" id="fecha" name='fecha'>
+            </div>
+            <div class="col-md-6">
+                <label for="horario" class="form-label">Hora</label>
+                <select id="horario" class="form-select" name='horario'>
+                  <option selected></option>
+                  <option value='20:00'>20:00 hs</option>
+                  <option value='20:30'>20:30 hs</option>
+                  <option value='21:00'>21:00 hs</option>
+                </select>
+              </div>
+            <div class="col-md-6">
+              <label for="categoria" class="form-label">Categoría</label>
+              <select id="categoria" class="form-select" name='categoria'>
+                <option selected></option>
+                <option value='pop'>Pop</option>
+                <option value='rock'>Rock</option>
+                <option value='trap'>Trap</option>
+              </select>
+            </div>
+            <div class="col-md-4">
+              <label for="stock" class="form-label">Stock</label>
+              <input type="number" class="form-control" id="stock" name="stock">
+            </div>
+            <div class="col-md-4">
+                <label for="estado" class="form-label">Estado</label>
+                <select id="estado" class="form-select" name='estado'>
+                  <option selected></option>
+                  <option value='en venta'>En venta</option>
+                  <option value='cancelado'>Cancelado</option>
+                  <option value='finalizado'>Finalizado</option>
+                </select>
+            </div>
+            <div class="col-md-4 input-group mb-3">
+                <span class="input-group-text">$</span>
+                <input type="text" class="form-control" aria-label="precio">
+            </div>
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary">Registrar evento</button>
+            </div>
+          </form>
+    </section>
+</body>
+</html>
