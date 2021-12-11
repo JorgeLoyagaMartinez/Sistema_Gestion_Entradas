8e82de6bc513925a57ee01d63db868fdbc836c86
Jorge Loyaga Martinez <JorgeLoyagaMartinez@users.noreply.github.com>
2021-12-11 15:48:26 -0300

    Update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $conexionMySQL = mysqli_connect("127.0.0.1:3306","root","","gestionentradas");
+    /*$conexionMySQL = mysqli_connect("127.0.0.1:3306","root","","gestionentradas");
     
     $resultado = mysqli_query($conexionMySQL,"SELECT * FROM usuario");
 
@@ -28,7 +28,25 @@
         echo "<br>";
     }
 
-    mysqli_close($conexionMySQL);
+    mysqli_close($conexionMySQL);*/
 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mi Primer Proyecto HTML!</title>
+        <link rel="stylesheet" href="css/estilos.css">
+    </head>
+    <body>
+        <h1 style="color: red; font-size:24px; text-align:center; text-decoration:underline;">Esto es un Titulo Principal</h1>
+        <h2 class="H2_Rojo H2_Grande" id="Primer_H2">Esto es un h2</h2>
+        <h2 class="H2_Rojo">Esto es otro h2</h2>
+        <p class="H2_Rojo">Esto es un Parrafo!</p>
+
+        </body>
+</html>
+
+<script>
+    var persona = {};
+    </script>
 
