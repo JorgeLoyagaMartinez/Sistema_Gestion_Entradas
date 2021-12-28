7ffd9f1ecb7bebd1d8429c49f5e7dea64b20bce7
pcostapaul <paladino.pablo@hotmail.com>
2021-12-28 16:08:11 -0300

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,15 +3,18 @@
 
     @section('content')
     <main>
-        <div class="welcome-container ingresar">
-            <div class="welcome-container-inner">
-                <div class="container">
-                    <div class="ntc-container text-center">
-                        <a role="button" href="#" onclick="window.history.go(-1); return false;" class="btn btn-link btn-block">Cerrar</a>
-                        <img src="img/darth.svg" alt="No tengo cuenta" aria-hidden="true">
-                        <h1 class="no-tengo-cuenta">¿Aún no tenés cuenta?</h1>
-                        <a role="button" href="registro-facebook.html" class="btn btn-primary btn-block">Registrarme con Facebook</a>
-                        <a role="button" href="registro.html" class="btn btn-primary btn-block">Registrarme con mi email</a>
+        <div class="d-flex justify-content-center align-items-center mt-5">
+            <div class="card">
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item text-center"> <a class="nav-link active btl" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Login</a> </li>
+                    <li class="nav-item text-center"> <a class="nav-link btr" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Signup</a> </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                        <div class="form px-4 pt-5"> <input type="text" name="" class="form-control" placeholder="Email or Phone"> <input type="text" name="" class="form-control" placeholder="Password"> <button class="btn btn-dark btn-block">Login</button> </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                        <div class="form px-4"> <input type="text" name="" class="form-control" placeholder="Name"> <input type="text" name="" class="form-control" placeholder="Email"> <input type="text" name="" class="form-control" placeholder="Phone"> <input type="text" name="" class="form-control" placeholder="Password"> <button class="btn btn-dark btn-block">Signup</button> </div>
                     </div>
                 </div>
             </div>
