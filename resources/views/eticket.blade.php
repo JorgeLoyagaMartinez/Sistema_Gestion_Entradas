8e5359eb996fc1d61994aeeef37d2158a2734a2d
pcostapaul <paladino.pablo@hotmail.com>
2021-12-29 01:15:03 -0300

    Log/Reg/Ticket

--- resources/views/eticket.blade.php
+++ resources/views/eticket.blade.php
@@ -4,50 +4,25 @@
 
 @section('content')
 <body>
-    <nav class="navbar sticky-top navbar-dark bg-primary">
 
-        <a role="button" class="btn-back" href="#" onclick="window.history.go(-1); return false;" aria-label="Volver">
-            <i class="fa fa-angle-left fa-2x" aria-hidden="true"></i>
-        </a>
-
-        <h1 class="navbar-brand">Tu E-ticket</h1>
-
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a role="button" class="nav-link" href="home.html" aria-label="Inicio">
-                    <i class="fa fa-home fa-2x" aria-hidden="true"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a role="button" class="nav-link" href="perfil-logueado.html" aria-label="Perfil">
-                    <i class="fa fa-user fa-2x" aria-hidden="true"></i>
-                </a>
-            </li>
-            <div class="tooltip-ticket">
-                El E-Ticket se guardó en tu perfil
-            </div>
-        </ul>
-    </nav>
 
     <main>
         <div class="container container-eticket">
-            <h2 class="text-center">¡Es hora de la película!</h2>
-            <p class="text-center">Entrá a la sala directamente presentando este E-Ticket digital, válido para todas las entradas compradas.</p>
+            <h2 class="text-center">¡Ya tenés tu entrada!</h2>
+            <p class="text-center">Entrá a la función directamente presentando este E-Ticket digital, válido para todas las entradas compradas.</p>
 
-            <div class="card eticket">
-                <div class="fanart">
-                    <img src="img/la-liga-de-la-justicia.jpg" alt="Se ve a los 5 superhéroes de la liga">
-                </div>
+            <div class="card eticket ticketcard">
+                
 
                 <div class="detalle-seleccion container">
                     <div class="row">
                         <div class="col-1">
                             <i class="fa fa-film" aria-hidden="true"></i>
-                            <span class="sr-only">Película: </span>
+                            <span class="sr-only">Evento:</span>
                         </div>
                         <div class="col-10">
                             <h3>La liga de la justicia</h3>
-                            <small>Subtitulada 2D</small>
+                            
                         </div>
                     </div>
                 </div>
@@ -60,8 +35,7 @@
                         </div>
                         <div class="col-10">
                             <h3>Hoyts Abasto</h3>
-                            <small>Av. Corrientes 3247, CABA</small>
-                        </div>
+                            </div>
                     </div>
                 </div>
 
@@ -72,7 +46,7 @@
                             <span class="sr-only">Fecha: </span>
                         </div>
                         <div class="col-5">
-                            <div class="fecha" aria-label="Sábado 28 de noviembre">
+                            <div class="fecha fechaevento" aria-label="Sábado 28 de noviembre">
                                 Sábado 28/11
                             </div>
                         </div>
@@ -87,24 +61,7 @@
                         </div>
                     </div>
                 </div>
-
-                <a role="button" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MGpyMmtoZzhqM3RxaTlvOGt0ZGliamlmNnEgbWFoZWVmdnVpazU4bGJsZ3FvZmFhOWtlNmdAZw&tmsrc=maheefvuik58lblgqofaa9ke6g%40group.calendar.google.com" class="btn btn-link">+ Agregar a mi calendario</a>
-
-                <div class="detalle-seleccion container">
-                    <div class="row">
-                        <div class="col-1">
-                            <i class="fa fa-braille" aria-hidden="true"></i>
-                        </div>
-                        <div class="col-10">
-                            <div class="eleccion-butacas">
-                                <span class="sr-only">Butacas</span>  4 y 5. Fila G. Sala 2
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <hr aria-hidden="true">
-
+                               
                 <div class="detalle-seleccion container">
                     <div class="row">
                         <div class="col-1">
@@ -112,7 +69,7 @@
                         </div>
                         <div class="col-10">
                             Valor total por 2 entradas: <strong aria-label="80 pesos">$80</strong> <br />
-                            <small>Con beneficio Club La Nación <span aria-label="2 por 1"></span> 2x1</small>
+                            
                         </div>
                     </div>
                 </div>
@@ -121,15 +78,8 @@
 
                 <img class="qr-code" src="img/qr-code.svg" alt="Código QR. Mostrá este código al personal de la sala y pasá directamente. Código alfanumérico: A136257JK">
 
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <a href="#" class="btn btn-link"><i class="fa fa-share" aria-hidden="true"></i> Compartir</a>
-                    <a href="#" class="btn btn-link"><i class="fa fa-download" aria-hidden="true"></i> Descargar</a>
-                </div>
+                
             </div>
-        </div>
-
-        <div class="container">
-            <a role="button" href="https://www.google.com.ar/maps/dir/Universidad+Tecnol%C3%B3gica+Nacional+-+Dpto+Ingenier%C3%ADa+Electr%C3%B3nica+-+UTN.BA,+Av.+Medrano+951,+C1190+CABA/Hoyts+Abasto,+Abasto+Shopping,+C1193AAE,+Av.+Corrientes+3247,+1193+CABA/@-34.6013499,-58.4201661,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x95bcca7cd5154d8f:0xceff1f97c5c94a26!2m2!1d-58.4199772!2d-34.5986601!1m5!1m1!1s0x95bcca8c8513598b:0x89b570ec39dc7d88!2m2!1d-58.4108358!2d-34.6034662!3e2?hl=en" class="btn btn-primary btn-block">Cómo llegar</a>
         </div>
     </main>
 
